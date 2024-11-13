21a2119695a559a4203e205b02097ffcffe290c8
dang1 <dustin1204@gmail.com>
2024-11-12 21:30:40 -0500

    Started routing to backend/database

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -27,7 +27,6 @@ export default async function ProtectedPage() {
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(user, null, 2)}
         </pre>
       </div>
       <div>
