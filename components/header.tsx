0ecc23e3facdb3b605f2fad75fd5294198e4f37d
Colin Heffernan <colinpheffernan@gmail.com>
2024-11-18 13:51:00 -0500

    Update styling and layout

---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="h-16 w-screen px-2 bg-slate-400 dark:bg-slate-900 flex flex-row justify-start items-center space-x-2">
+            <p className="h-12 px-2 rounded-lg bg-slate-300 dark:bg-slate-800 text-slate-800 dark:text-slate-300 text-center align-middle">Pantry of Pangaea</p>
+            <p className="h-12 px-2 rounded-lg bg-slate-300 dark:bg-slate-800 text-slate-800 dark:text-slate-300 text-center align-middle">Some login button idk</p>
+        </div>
+    );
+}
