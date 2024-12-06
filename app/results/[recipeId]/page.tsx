6be9b964f1ae97e6b3bc32331ad52b90ff0f8a1a
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-05 19:21:28 -0500

    Lay out recipe/results nicer, track rates and tags

--- app/results/[recipeId]/page.tsx
+++ app/results/[recipeId]/page.tsx
@@ -1,4 +1,4 @@
-import { getRecipe, getReview } from "@/app/actions";
+import { getRecipe, getReview, getUser } from "@/app/actions";
 import PostReview from "@/components/review";
 import { createClient } from "@/utils/supabase/server";
 import {postReview} from "@/app/actions"
@@ -28,37 +28,65 @@ export default async function RecipeDetails({ params }: { params: { recipeId: st
             throw new Error(recipeError);
         }
         else if(!reviewData || reviewError ) {
-            throw new Error(reviewError);
+            // throw new Error(reviewError);
         }
         
         //Chooses the first item in the returned object
         const recipe = recipeData[0];
 
+        const { data: userData, error: userError } = await getUser(recipe.author);
+        let authorName = "Unknown user";
+        if(userData && !userError && userData.length == 1) {
+            const user = userData[0];
+            authorName = user.display_name;
+        }
 
         // Return the recipe details and mapping it through
         return (
             <div>
-                <h1>{recipe.title}</h1>
-                <h3>Ingredients:</h3>
-                <ul>    
+                <h1 className="text-3xl font-bold">{recipe.title}</h1>
+                <h5 className="text-lg font-normal"><i>Posted by <u>{authorName}</u></i></h5>
+                <br />
+                <h3 className="text-2xl font-semibold">Ingredients:</h3>
+                <ul className="list-disc list-inside">    
                     {recipe.ingredients.map((ingredient : string, index : number) => (
                         <li key={index}>{ingredient}</li>
                     ))}
                 </ul>
-                <h4>Instructions:</h4>
-                <ol key="Instructions">
+                <br />
+                <h3 className="text-2xl font-semibold">Instructions:</h3>
+                <ol className="list-decimal list-inside">
                     {recipe.instructions.map((instruction: string, index : number) => (
                         <li key={index}>{instruction}</li>
                     ))}
                 </ol>
-                <PostReview formAction={postReview} recipeId={decodeURIComponent(recipeId)} />        
-                <h3>Reviews:</h3>
-                <ul>
-                    {reviewData?.map((review: any, index: number) => (
-                        <li key={index}>{review.content} {review.rating}</li> // Assuming review has a `comment` field
+                <br />
+                <h3 className="text-2xl font-semibold">Tags:</h3>
+                <ul className="list-disc list-inside">    
+                    {recipe.tags.map((tag: string, index : number) => (
+                        <li key={index}>{tag}</li>
                     ))}
-                </ul>   
-                 </div>
+                </ul>
+                <br />
+                <PostReview formAction={postReview} recipeId={decodeURIComponent(recipeId)} />        
+                <br />
+                <h3 className="text-2xl font-semibold">Reviews:</h3>
+                <h5 className="text-lg font-normal"><i>Average score: <u>{Math.trunc(recipe.average_rating * 10) / 10}/5 across {recipe.num_ratings} reviews</u></i></h5>
+                {reviewData && !reviewError && <ul>
+                    {reviewData.filter((review: {deleted: boolean}) => !review.deleted).map(async (review: {reviewer_id: string, content: string, rating: number}, index: number) => {
+                        const { data: userData, error: userError } = await getUser(review.reviewer_id);
+                        let reviewerName = "Unknown user";
+                        if(userData && !userError && userData.length > 0) {
+                            const user = userData[0];
+                            reviewerName = user.display_name;
+                        }
+                        return <li key={index}>{reviewerName} ({review.rating}/5): {review.content}</li> // Assuming review has a `comment` field
+                    })}
+                </ul>}
+                {(!reviewData || reviewError) && <div>
+                    There are no reviews yet. Will you be the first?
+                </div>} 
+            </div>
         );
     } catch (err : any) {
         return <h1>Error: {err.message}</h1>;
--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@/utils/supabase/server';
+import { getUser } from '@/app/actions';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link'
 
@@ -28,30 +29,36 @@ export default async function Results(props: {
   if (error)
     return <main>We couldn't reach the database. Please try again later.</main>;
 
-  return data != null ? ( 
+  return (data != null) ? ( 
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        {data.map((recipe, index) => (
-          <div key={recipe.id}>
-            {index != 0 && <hr />}
-            <Link href={`results/${encodeURIComponent(recipe.id)}`}>
-            {recipe.title}
-          </Link>
-            <h3>Ingredients:</h3>
-            <ul>
-
-              {recipe.ingredients.map((ingredient: string, index: number) => (
-                <li key={index}>{ingredient}</li>
-              ))}
-            </ul>
-            <h3>Instructions:</h3>
-            <ol>
-              {recipe.instructions.map((instruction: string, index: number) => (
-                <li key={index}>{instruction}</li>
-              ))}
-            </ol>
-          </div>
-        ))}
+        <h1 className="text-3xl font-bold">Results:</h1>
+        {data.map(async (recipe) => {
+          const { data: userData, error: userError } = await getUser(recipe.author);
+          let authorName = "Unknown user";
+          if(userData && !userError && userData.length > 0) {
+            const user = userData[0];
+            authorName = user.display_name;
+          }
+          let ratingText = "Unrated";
+          if(recipe.num_ratings > 0) {
+            ratingText = `Rated ${Math.trunc(recipe.average_rating * 10) / 10}/5 by ${recipe.num_ratings} Chef`;
+            if(recipe.num_ratings > 1) ratingText += 's';
+          }
+          return (
+            <div key={recipe.id}>
+              <h3 className="text-2xl font-bold">
+                <Link href={`results/${encodeURIComponent(recipe.id)}`}>
+                  <u>{recipe.title}</u>
+                </Link>
+              </h3>
+              <h5 className="text-lg font-normal">
+                <i>Posted by <u>{authorName}</u>
+                <br />{ratingText}</i>
+              </h5>
+            </div>
+          );
+      })}
       </main>
     </>
   ) : (
