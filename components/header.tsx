c9f23fdb5fc44c87821bbd8f4e5625770e67b6ba
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-06 13:50:32 -0500

    Remove dead code

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import SearchBar from "@/components/search-bar";
 
@@ -22,18 +21,6 @@ export default async function Header() {
 
         {/* Search Bar */}
         <SearchBar />
-        {/*
-        <div className="flex-grow mx-4">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Search recipes..."
-              className="w-full px-4 py-2 rounded-lg bg-gray-700 text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500"
-            />
-            <button type="submit" style={{display: "none"}} />
-          </form>
-        </div>
-        */}
 
         {/* Navigation Area */}
         <div className="flex-grow flex justify-end items-center space-x-6 text-lg">
@@ -56,29 +43,6 @@ export default async function Header() {
               Log Out
             </Link>
           </>}
-
-          {/* Search Button */}
-          {/*
-          <div
-            className={`hover:cursor-pointer transition-all duration-500 ${
-              isSearchActive ? "opacity-0 pointer-events-none" : ""
-            }`}
-            onMouseEnter={() => setIsSearchActive(true)} // Activate search on hover
-          >
-            Search
-          </div>
-          */}
-
-          {/* Search Bar */}
-          {/*
-          <div className="absolute right-4 w-[400px] transition-transform duration-500 ease-in-out">
-            <input
-              type="text"
-              placeholder="Search recipes..."
-              className="w-full px-4 py-2 bg-white text-gray-800 border border-yellow-400 rounded-md shadow-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 transition-all duration-300"
-            />
-          </div>
-          */}
         </div>
       </nav>
     </header>
